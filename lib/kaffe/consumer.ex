7c72f99d3d8f568561844b355478760e4bcb5621
Stephen Ball <sdball@gmail.com>
2016-10-19 09:51:46 -0400

    Pass the pid to the handlers for async processing
    Instead of getting weird with registering global processes for the
    running consumer we'll instead take a smarter approach of passing the
    pid to the handler along with the message. The handler will do its async
    processing and then call back to Consumer with the pid and message to
    have the offset committed to Kafka.

--- lib/kaffe/consumer.ex
+++ lib/kaffe/consumer.ex
@@ -1,6 +1,16 @@
 defmodule Kaffe.Consumer do
   @moduledoc """
   Consume messages from Kafka and pass to a given local module.
+
+  See `start_link/5` for details on how to start a Consumer process.
+
+  As messages are consumed from Kafka they will be sent to your
+  `handle_message/1` (sync) or `handle_message/2` (async) functions for
+  processing in your system. Those functions _must_ return `:ok`.
+
+  Kaffe.Consumer commits offsets to Kafka which is very reliable but not
+  immediate. If your application restarts then it's highly likely you'll
+  reconsume some messages, especially for a quickly moving topic. Be ready!
   """
 
   @behaviour :brod_group_subscriber
@@ -14,7 +24,7 @@ defmodule Kaffe.Consumer do
     Running state for the consumer.
 
     - `message_handler`: The module to call with each Kafka message
-    - `async`: How to acknowledge Kafka messages as processed
+    - `async`: Kafka offset processing behavior
     """
     defstruct message_handler: nil, async: false
   end
@@ -30,9 +40,16 @@ defmodule Kaffe.Consumer do
     - `consumer_group`: the consumer group id (should be unique to your app)
     - `topics`: the list of Kafka topics to consume
     - `message_handler`: the module that will be called for each Kafka message
-    - `async`: if false then Kafka messages will be acknowledged after handling is complete
+    - `async`: if false then Kafka offsets will not be automatically acknowledged
 
-  Note: If `async` is true then you'll need call `ack/4` to acknowledge offsets per topic and partition after messages are processed.
+  Note: If `async` is true then you'll need to call `ack/2` to acknowledge
+  Kafka messages as processed.
+
+  Only use async processing if absolutely needed by your application's
+  processing flow. With automatic (sync) acknowledgement then the message flow
+  from Kaffe.Consumer has backpressure from your system. With manual (async)
+  acknowledgement you will be able to process messages faster but will need to
+  take on the burden of ensuring no message is lost.
   """
   def start_link(client, consumer_group, topics, message_handler, async) do
     group_config = [offset_commit_policy: :commit_to_kafka_v2, offset_commit_interval_seconds: 5]
@@ -43,26 +60,18 @@ defmodule Kaffe.Consumer do
   end
 
   @doc """
-  Commit the offset to Kafka for the topic/partition and group consumer.
+  Acknowledge the Kafka message as processed.
 
-  You can either pass the pid of the running Consumer or the group consumer id.
-
-  e.g.
+  - `pid`: the pid your `handle_message/2` function was given as the first argument
+  - `message`: the Kafka message your `handle_message/2` function was given as
+    the second argument
 
   ```
-  Kaffe.Consumer.ack(pid, "commitlog", 0, 124)
-
-  Kaffe.Consumer.ack("index-consumer-group", "commitlog", 0, 124)
+  Kaffe.Consumer.ack(pid, message)
   ```
   """
-  def ack(pid, topic, partition, offset) when is_pid(pid) do
+  def ack(pid, %{topic: topic, partition: partition, offset: offset}) do
     :brod_group_subscriber.ack(pid, topic, partition, offset)
-  end
-
-  def ack(group_consumer, topic, partition, offset) do
-    group_consumer
-    |> String.to_atom
-    |> :brod_group_subscriber.ack(topic, partition, offset)
   end
 
   ## -------------------------------------------------------------------------
@@ -72,15 +81,36 @@ defmodule Kaffe.Consumer do
   @doc """
   Initialize the consumer loop.
   """
-  def init(consumer_group, [message_handler, async]) do
-    Process.register(self, String.to_atom(consumer_group))
+  def init(_consumer_group, [message_handler, async]) do
     {:ok, %Kaffe.Consumer.State{message_handler: message_handler, async: async}}
   end
 
   @doc """
-  Call the message handler with the Kafka message.
+  Call the message handler with the restructured Kafka message.
 
-  If `async` is true you'll need to manually call `ack/4` to acknowledge messages as processed per topic and partition.
+  Kafka messages come from brod as an Erlang record. To make processing simpler
+  for clients we convert that to an Elixir map. Since the consumer can
+  subscribe to multiple topics with multiple partitions we also add the topic
+  and partition as additional fields.
+
+  After compiling the Kafka message your message handler module's
+  `handle_message` function will be called.
+
+  If `async` is false:
+
+  - Your message handler module's `handle_message/1` function will be called
+    with the message
+  - The Consumer will block and wait for your `handle_message` function to
+    complete and then automatically acknowledge the message as processed.
+
+  If `async` is true:
+
+  - Your message handler module's `handle_message/2` function will be called
+    with the pid of the running Consumer process and the message.
+  - Message intake on the Consumer will not wait for your `handle_message/2` to
+    complete and will not automatically acknowledge the message as processed.
+  - Once you've processed the message you will need to call
+    `Kaffe.Consumer.ack/2` with the pid and message.
   """
   def handle_message(topic, partition, msg, %{async: false, message_handler: handler} = state) do
     :ok = apply(handler, :handle_message, [compile_message(msg, topic, partition)])
@@ -88,7 +118,7 @@ defmodule Kaffe.Consumer do
   end
 
   def handle_message(topic, partition, msg, %{async: true, message_handler: handler} = state) do
-    :ok = apply(handler, :handle_message, [compile_message(msg, topic, partition)])
+    :ok = apply(handler, :handle_message, [self, compile_message(msg, topic, partition)])
     {:ok, state}
   end
 
