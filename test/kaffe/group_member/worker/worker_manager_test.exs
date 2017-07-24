fb69aa1fccfdb216593cd3537e312e9db1a0bde9
Kevin Lewis <kevin@spreedly.com>
2017-07-24 11:39:03 -0600

    Fix test failures
    One was due to test ordering, the other due to name mismatches.
    
    Bumped version to 1.2.1.

--- test/kaffe/group_member/worker/worker_manager_test.exs
+++ test/kaffe/group_member/worker/worker_manager_test.exs
@@ -6,9 +6,9 @@ defmodule Kaffe.WorkerManagerTest do
   defmodule TestWorkerSupervisor do
     def start_worker(_, _, _, worker_name) do
       case worker_name do
-        :"kaffe_worker_0" -> {:ok, 1}
-        :"kaffe_worker_topic1_0" -> {:ok, 1}
-        :"kaffe_worker_topic2_0" -> {:ok, 2}
+        :"worker_0" -> {:ok, 1}
+        :"worker_topic1_0" -> {:ok, 1}
+        :"worker_topic2_0" -> {:ok, 2}
       end
     end
   end
@@ -16,12 +16,11 @@ defmodule Kaffe.WorkerManagerTest do
   setup do
     Application.put_env(:kaffe, :worker_supervisor_mod, TestWorkerSupervisor)
     {:ok, worker_manager_pid} = WorkerManager.start_link("worker_test")
+    on_exit fn -> configure_strategy(:worker_per_partition) end
     %{worker_manager_pid: worker_manager_pid}
   end
 
   test "allocate worker per partition", %{worker_manager_pid: worker_manager_pid} do
-    configure_strategy(:worker_per_partition)
-
     worker_pid1 = WorkerManager.worker_for(worker_manager_pid, "topic1", 0)
     worker_pid2 = WorkerManager.worker_for(worker_manager_pid, "topic2", 0)
 
