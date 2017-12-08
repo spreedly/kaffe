f41407d996d462d4d0fb021186841a1e8cb29a59
Damien Krotkine <3834+dams@users.noreply.github.com>
2017-12-08 10:15:49 +0100

    Minor updates to README examples
    Document message timestamp and timestamp type, and fix the example.
    
    Bump version to 1.4.2
    
    Closes #50

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Kaffe.Mixfile do
 
   def project do
     [app: :kaffe,
-     version: "1.4.1",
+     version: "1.4.2",
      description: "An opinionated Elixir wrapper around brod, the Erlang Kafka client, that supports encrypted connections to Heroku Kafka out of the box.",
      name: "Kaffe",
      source_url: "https://github.com/spreedly/kaffe",
