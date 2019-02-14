55c230e27fc25b6cac66635ddca9a062d1203baa
Marina Krasnova <marinakrasnovatr81@gmail.com>
2019-02-14 19:17:18 +0200

    feat: add support for SASL_PLAINTEXT auth for kafka broker

--- config/test.exs
+++ config/test.exs
@@ -15,9 +15,10 @@ config :kaffe,
     rebalance_delay_ms: 100,
     max_bytes: 10_000,
     subscriber_retries: 1,
-    subscriber_retry_delay_ms: 5,
+    subscriber_retry_delay_ms: 5
   ],
   producer: [
-    endpoints: [kafka: 9092],
-    topics: ["kaffe-test"]
+    endpoints: [localhost: 9092],
+    topics: ["kaffe-test"],
+    sasl: {:plain, "KAFFE_PRODUCER_USER", "KAFFE_PRODUCER_PASSWORD"}
   ]
