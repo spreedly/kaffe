320b4482a6038c35ad49205edc2e7165336dd29e
Anthony Walker <anthony@spreedly.com>
2024-10-09 13:32:05 -0400

    Upgrade to Elixir 1.14
    Upgrades the Elixir version to 1.14
    
    mix test
    4 doctests, 47 tests, 0 failures, 1 excluded
    
    mix test --only e2e
    4 doctests, 47 tests, 0 failures, 50 excluded

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 config :kaffe,
   kafka_mod: TestBrod,
--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Kaffe.Mixfile do
       app: :kaffe,
       version: @version,
       name: "Kaffe",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
